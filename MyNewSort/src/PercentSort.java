af937496284862b526018194705b1b2d71d85714
amgazda <67490541+amgazda@users.noreply.github.com>
2022-07-06 12:10:24 -0400

    Add new sort

---
+++ MyNewSort/src/PercentSort.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class PercentSort {
+    private ArrayList<Integer> arr;
+
+    public PercentSort() {
+        arr = new ArrayList<>();
+    }
+
+    public void add(int a) {
+        //add check for a == 0
+        int max = arr.get(arr.size()-1);
+        int min = arr.get(0);
+        int size = arr.size();
+        double rmax = (max-a)/max;
+        double rmin = (a-min)/a;
+        int mid;
+        if (rmax < rmin) {
+            mid = (int) Math.ceil(rmax*size);
+            if (mid>(size-1)) {mid = size-1;}
+        } else {
+            mid = (int) Math.floor(rmin*size);
+            if (mid < 0) {mid = 0;}
+        }
+    }
+}
